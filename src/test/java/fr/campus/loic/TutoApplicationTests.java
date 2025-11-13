d2837215170792f1070118afaf7403d5f53822fc
loic jobard <loic-jobard@le-campus-numerique.fr>
2025-11-13 10:11:22 +0100

    first commit

---
+++ src/test/java/fr/campus/loic/TutoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.campus.loic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
